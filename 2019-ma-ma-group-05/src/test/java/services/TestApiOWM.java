cd6d0980bde5f96a60a0884c4312a1a09452d63e
SantiagoIvan <santiago.feijoo96@gmail.com>
2019-05-15 17:34:44 -0300

    Api OWM integrada, con archivo de configuraciones

--- 2019-ma-ma-group-05/src/test/java/services/TestApiOWM.java
+++ 2019-ma-ma-group-05/src/test/java/services/TestApiOWM.java
@@ -1,5 +1,11 @@
 package services;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
 import dtoClases.ClimaDto;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -11,18 +17,31 @@ import services.RetrofitClimaService;
 public class TestApiOWM {
 
 	public static void main(String[] args) {
+    	Properties archivoDeConfiguraciones= new Properties();
+    	InputStream input=null;
+    	try{
+            input = new FileInputStream("configuraciones.properties");
+            archivoDeConfiguraciones.load(input);
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Error cargando configuración\n" + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    	String urlBase= archivoDeConfiguraciones.getProperty("endpointOwm");
+    	String appid=archivoDeConfiguraciones.getProperty("appid");
+    	String units=archivoDeConfiguraciones.getProperty("units");
+    	String cabaId=archivoDeConfiguraciones.getProperty("cabaId");
+    	
 		Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.openweathermap.org/")
+                .baseUrl(urlBase)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         RetrofitClimaService service = retrofit.create(RetrofitClimaService.class);
         
-        Call<ResponseClima> call = service.getClimaByCityId(3435907);
+        Call<ResponseClima> call = service.getClimaByCityId(cabaId,units,appid);
         
         try{
             Response<ResponseClima> response = call.execute();
-            ResponseClima clima = response.body();
-            System.out.print(clima.main.temperatura());
+            ClimaDto clima = response.body().main;
+            System.out.print(clima.temperatura());
         }
         catch (Exception ex){
             System.out.print(ex.getMessage());
