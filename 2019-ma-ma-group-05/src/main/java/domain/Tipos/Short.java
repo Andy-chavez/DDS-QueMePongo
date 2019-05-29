523a919e616e96d30c19bb49c0a65927569a00c9
Andy-chavez <32040585+Andy-chavez@users.noreply.github.com>
2019-05-29 15:10:33 -0300

    Add files via upload

--- 2019-ma-ma-group-05/src/main/java/domain/Tipos/Short.java
+++ 2019-ma-ma-group-05/src/main/java/domain/Tipos/Short.java
@@ -6,9 +6,9 @@ import domain.Categoria;
 import domain.FamiliaTipos;
 import domain.Tela;
 public class Short implements FamiliaTipos{
-	public String tipo = "short";
-	public EnumSet<Tela> telasPosibles = EnumSet.of(Tela.ALGODON,Tela.NYLON,Tela.POLYESTER,Tela.SEDA,Tela.OTRO);
-	public Categoria categoria = Categoria.INFERIOR;
+	private String tipo = "short";
+	private EnumSet<Tela> telasPosibles = EnumSet.of(Tela.ALGODON,Tela.NYLON,Tela.POLYESTER,Tela.SEDA,Tela.OTRO);
+	private Categoria categoria = Categoria.INFERIOR;
 	
 	public String getTipo() {
 		return tipo;
