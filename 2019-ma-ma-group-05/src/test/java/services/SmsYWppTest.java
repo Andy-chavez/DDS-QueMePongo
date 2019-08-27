bb6f564542b2b9913bf77b9f65e728de32c19769
Andy-chavez <andychavez02@gmail.com>
2019-08-26 21:15:12 -0300

    ya podemos mandar mails

--- 2019-ma-ma-group-05/src/test/java/services/SmsYWppTest.java
+++ 2019-ma-ma-group-05/src/test/java/services/SmsYWppTest.java
@@ -6,6 +6,7 @@ public class SmsYWppTest { //es super precario, ya se, pero dps va a ir mejorand
 	
 	private SmsSender sms = new SmsSender();
 	private WppSender wpp = new WppSender();
+	private EmailSender email = new EmailSender();
 	@Test
 	public void seEnvioSms() {
 		sms.configurar();
@@ -15,5 +16,9 @@ public class SmsYWppTest { //es super precario, ya se, pero dps va a ir mejorand
 	public void seEnvioWpp() {
 		wpp.configurar();
 		wpp.enviar("Hola! Cómo va? Este es un wpp");
+	}
+	@Test
+	public void seEnvioEmail() { //aca temporalmente van a tener que creer ciegamente que funciona, pero dps nos hacemos un mail y lo usamos
+		email.enviar("xxxxx@gmail.com","contraXXXXX","andychavez02@gmail.com","Buenas","Intento Funciona");
 	}
 }
