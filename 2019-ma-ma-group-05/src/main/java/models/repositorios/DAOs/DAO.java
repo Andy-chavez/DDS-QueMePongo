f3a77af46a7ecf8660cd59adf2f69f80a2c90c76
Matias <matijurado@est.frba.utn.edu.ar>
2019-10-29 15:58:34 -0300

    actualizo repos

--- 2019-ma-ma-group-05/src/main/java/models/repositorios/DAOs/DAO.java
+++ 2019-ma-ma-group-05/src/main/java/models/repositorios/DAOs/DAO.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface DAO {
     public Object buscarPorId(int id);
     public Object buscarPorNombre(String nombre);
-    public List<Object> buscarTodos();
+    public List<?> buscarTodos();
     public void modficarPorId(int id, Object o);
     public void eliminar(Object o);
     public void agregar(Object o);
